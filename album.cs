db6b78d9b38c4dd66aa28fab340822e78e2805d8
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-10-27 10:15:14 -0300

    Add files via upload

---
+++ album.cs
@@ -0,0 +1,33 @@
+﻿using csharp2_project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Album
+{
+    private List<musica> musicas = new List<musica>();
+
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do álbum {Nome}:\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música: {musica.nome}");
+        }
+        Console.WriteLine($"\nPara ouvir este álbum inteiro você precisa de {DuracaoTotal}");
+    }
+}
